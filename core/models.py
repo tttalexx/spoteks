f90df551474148eeb313b9a6705da90e6b7a1099
tttalexx <130794162+tttalexx@users.noreply.github.com>
2024-08-23 00:45:16 +0100

    db_table = 'bd_user'  # Указываем имя таблицы явно

--- core/models.py
+++ core/models.py
@@ -1,8 +1,5 @@
-# core/models.py
-
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import PermissionsMixin
 
 class UserManager(BaseUserManager):
     def create_user(self, phone_number, password=None, **extra_fields):
@@ -23,7 +20,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     user_id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=12, unique=True)  # В формате 380XXXXXXX
+    phone_number = models.CharField(max_length=12, unique=True)
     email = models.EmailField(max_length=100, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -42,5 +39,8 @@ class BDUser(models.Model):
     user_email = models.EmailField(max_length=100)
     user_role = models.CharField(max_length=50, default="user")
 
+    class Meta:
+        db_table = 'bd_user'  # Указываем имя таблицы явно
+    
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
