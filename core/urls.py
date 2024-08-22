682047dbc427cc4426a687a4c42c763e7d0748a6
tttalexx <130794162+tttalexx@users.noreply.github.com>
2024-08-22 23:28:50 +0100

    1

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,11 @@
 # core/urls.py
 
 from django.urls import path
-from .views import register_view, login_view, logout_view
+from .views import register_view, login_view, logout_view, home_view
 
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('', home_view, name='home'),  # Добавляем маршрут для домашней страницы
+    path('', home_view, name='home'),  # Маршрут для домашней страницы
 ]
